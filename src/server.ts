9e2afdc200a44fdccc81a10d178ebbb2b2866dbe
Kalu <kalu.dematto@gmail.com>
2023-04-11 17:03:43 -0300

    few changes on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express"
+import { Request, Response } from 'express';
 
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+const app = express();
+const port = 3000;
 
-app.get('/', (request: Request, response:Response) => {
-  response.send('Hello World!')
-})
+app.get('/', (request: Request, response: Response) => {
+    response.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
