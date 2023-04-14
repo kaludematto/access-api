7675673349a71b745ea3d0a3399d6cc11bf1ac83
Kalu <kalu.dematto@gmail.com>
2023-04-14 13:35:43 -0300

    create user entity and interface

---
+++ src/infra/repository/typeorm/models/user.entity.ts
@@ -0,0 +1,47 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { IUser } from './dto/IUser';
+
+@Entity()
+export class User implements IUser {
+    @PrimaryGeneratedColumn()
+    id?: number | undefined;
+
+    @Column({ nullable: true })
+    name!: string;
+
+    @Column({ nullable: true })
+    email!: string;
+
+    @Column({ nullable: true })
+    password!: string;
+
+    @Column({ nullable: true })
+    document!: string;
+
+    @Column({ nullable: true })
+    picture!: string;
+
+    @Column({ nullable: true })
+    phone!: string;
+
+    @Column({ nullable: true })
+    job!: string;
+
+    @Column({ nullable: true })
+    can_approve!: boolean;
+
+    @Column({ nullable: true })
+    auth!: string;
+
+    @CreateDateColumn()
+    created_at?: Date | undefined;
+
+    @UpdateDateColumn()
+    updated_at?: Date | undefined;
+}
