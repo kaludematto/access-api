85ba832e0dfe5e7e6fe599d483e7bfe50f07c968
Kalu <kalu.dematto@gmail.com>
2023-04-24 12:07:08 -0300

    fix: fixed routes config

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -3,7 +3,8 @@ import { Express, Router } from 'express';
 
 export default function setupRoutes(app: Express): void {
     const router = Router();
-    router.use('/access-api/', createUserRoute);
+    router.use('/access-api/', router);
+    router.use(createUserRoute);
     router.use(getUserRoute);
     app.use(router);
 }
